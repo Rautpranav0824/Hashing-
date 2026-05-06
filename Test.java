f9fde4c80e6e651288f8f2c01b6fbf832d846661
Pranav Raut <pranavraut.dev@gmail.com>
2026-05-06 16:46:29 +0530

    chore: remove unnecessary files

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        int[] hash = new int[10];
-        String abc = "abc";
-        char ch = abc.charAt(0);
-        hash[ch-'a']=5;
-        System.out.println(hash[ch-'a']);
-    }
-}
